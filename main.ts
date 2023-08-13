decb143abf670e5d7d47c6896f76e5460b47f6d3
DariaKostrik <dolorez0927@yandex.ru>
2023-08-13 23:41:09 +0300

    add animals and aviaries

--- main.ts
+++ main.ts
@@ -1,21 +1,101 @@
-type Biom = "desert"|"tropics"|"ocean"
+type Biom = "desert"|"tropics"|"tundra"
 interface Aviary {
-
+    
     biom: Biom,
     sizeOfAviary: number,
     reservoir:boolean; 
+}
+
+
+const Desert: Aviary ={
+    biom: "desert",
+    sizeOfAviary: 40,
+    reservoir:true
+}
+const TropicsOne: Aviary ={
+    biom: "tropics",
+    sizeOfAviary: 100,
+    reservoir:true
+}
+const TropicsTwo: Aviary ={
+    biom: "tropics",
+    sizeOfAviary: 80,
+    reservoir:true
 }
 
 interface Animal {
     species: string,
     biom: Biom,
     reservoir: boolean;
-    SizeOfAviaryPerAnimal: number;
+    sizeOfAviaryPerAnimal: number;
     food: string[];
     predator: boolean;
 }
 
-interface Сreature{
-    name: string;
+interface Species{
+    name: string|undefined ;
     dailyFoodNorm: number;
+}
+
+
+let SaigarOne: Animal&Species ={
+    species:"saiga",
+    biom: "desert",
+    reservoir: false,
+    sizeOfAviaryPerAnimal: 22,
+    food: ["grass"],
+    predator: false,
+    name: "Bob",
+    dailyFoodNorm: 1.2
+}
+let Сamel: Animal& Species ={
+    species:"camel",
+    biom: "desert",
+    reservoir: false,
+    sizeOfAviaryPerAnimal: 10,
+    food: ["grass"],
+    predator: false,
+    name: undefined,
+    dailyFoodNorm: 3
+}
+
+    let CrocodileOne: Animal& Species ={
+    species:"crocodile",
+    biom: "tropics",
+    reservoir: true,
+    sizeOfAviaryPerAnimal: 30,
+    food: ["meat","burds","fish"],
+    predator: true,
+    name: "Kiki",
+    dailyFoodNorm: 1
+}
+let CrocodileTwo: Animal&Species ={
+    species: "crocodile",
+    biom: "tropics",
+    reservoir: true,
+    sizeOfAviaryPerAnimal: 30,
+    food: ["meat","burds","fish"],
+    predator: true,
+    name: "Tom",
+    dailyFoodNorm: 1
+}
+let ElephantOne: Animal&Species ={
+    species:"elephant",
+    biom: "tropics",
+    reservoir: false,
+    sizeOfAviaryPerAnimal: 25,
+    food: ["fruit","vegetables"],
+    predator: false,
+    name: "Ella",
+    dailyFoodNorm: 30
+}
+let ElephantTwo: Animal&Species ={
+    species:"elephant",
+    biom: "tropics",
+    reservoir: false,
+    sizeOfAviaryPerAnimal: 25,
+    food: ["fruit","vegetables"],
+    predator: false,
+    name: "Eigon",
+    dailyFoodNorm: 30
 }
